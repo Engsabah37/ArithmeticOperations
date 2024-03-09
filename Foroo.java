6bd1fd1434711f8f015da69f9c09b0d7f591dd4c
Engsabah37 <96893085+Engsabah37@users.noreply.github.com>
2024-03-09 19:30:29 +0200

    Add files via upload

---
+++ Foroo.java
@@ -0,0 +1,71 @@
+package com.mycompany.foroo;
+
+import java.util.Scanner;
+
+public class Foroo {
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+/*int grade , sum=0;
+        for (int counter = 1; counter <=6; counter++) 
+        {
+            System.out.println("enter grade for student no" + counter);
+            grade = input.nextInt();
+            sum+=grade;
+        }
+        System.out.println("the average is" + (sum/6));*/
+
+    //print out even number
+        /*for (int i = 0; i < 10; i+=2)
+           System.out.println(i);*/ 
+        
+    //decrement
+       /* for (int i =10; i <=10; i++)
+            System.out.println(i);*/
+        
+      /*int i=0;
+        for(;++i<=10;)
+            System.out.println(i);*/
+        
+    //the accepts 10 Numbers form the user and print out 
+       //the sum of even numbers AND the sum of odd numbers
+    
+    /*int num, sumeven=0, sumodd=0;
+        for (int i = 0; i <= 10; i++) 
+        {
+            System.out.println("enter number" + i);
+            num = input.nextInt();
+            if(num%2==0)
+                sumeven+=num;
+            else
+                sumodd+=num;
+        }
+        System.out.println("the sum of even numbers is " +sumeven );
+        System.out.println("the sum of even numbers is " +sumodd );*/
+    
+    /* System.out.println("enter number to get its factorial");
+    int number = input.nextInt();
+    int factorial =1;
+    //int factorial =0;
+        for (int i = 1 ; i <=number; i++)
+        //for (int i = 0 ; i <=number; i++)
+        {
+          factorial =factorial*i;
+        }
+        System.out.println("factorial is "+factorial);*/
+    
+    //in nested loop
+        /*for (int j=1; j<=5; j++)
+        {
+         System.out.println("enter number to get its factorial");
+        int number = input.nextInt();
+        int factorial =1;
+                for (int i = 1 ; i <=number; i++)
+                {
+                  factorial =factorial*i;
+                }
+            System.out.println("factorial is "+factorial);
+        }*/
+    }
+}
